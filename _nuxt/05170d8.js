6ca71b6dafd9e12304c127dac4b6bb483e9e9acc
Sean Chen <Sean.Chen@leocorn.com>
2021-10-24 10:53:04 -0400

    remove some no need pages

---
+++ _nuxt/05170d8.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{514:function(n,o,l){"use strict";l.r(o);var e=l(5),component=Object(e.a)({},void 0,void 0,!1,null,null,null);o.default=component.exports}}]);
